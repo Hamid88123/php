a44f1cf14507d8874bcd5990544525a5788b8054
Hamid88123 <hamid.asad786@gmail.com>
2022-08-17 09:39:35 +1000

    php code added

---
+++ test.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>PHP LEARNING FROM SCRATCH</title>
+  </head>
+  <body>
+  <?php
+    echo '<h1>PHP LEARNING FROM SCRATCH BEST OF LUCK HAMID</h1>';
+/////////// variables in Php/////////////////
+
+    $variable1 =34;
+    $variable2 =34.5;
+    $variable3 ='i am hamid';
+    // echo $variable1;
+    // echo '</br>';
+    // echo $variable2;
+    // echo '</br>';
+    // echo $variable3;
+
+    //echo 'Value of first varaible is:'.$variable1.'</br>Value of Second varaible is:'.$variable2.'</br>Value of Third varaible is:'.$variable3;
+
+
+
+    ///////// Arithmetic Operator (+-*/)////////////
+
+    // $variable3= $variable1/$variable2;
+    // echo $variable3;
+
+    /////////////// Assignment Operator//////////////
+    // $newVar=$variable1;
+    // echo $variable1.'</br>';
+    // echo $newVar+=1;
+    // echo '</br>';
+    // echo $newVar-=1;
+
+    /////////////// Comparison Operator///////////
+                   //(=,!=,>=,<=)////
+
+    ////////////// Increment and Decrement Operators////////////
+    //// Post increment  operator////
+    // $new=5;
+    //  $new++;
+    // echo $new;
+
+    ///////// Pre increment OPerator/////////
+    // $new1=5;
+    // echo --$new1;
+
+    ////////// Constant in PHP //////////
+    define('hamid',20);
+    echo hamid;
+
+
+
+    ?>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
